22abb74b9249a30d77daf20315f3f39f4206b85b
prodriguezsosa <pedro.rodriguezsosa@gmail.com>
2018-06-18 11:21:37 -0400

    Extract date
    - check if first line of text has a date, if so, extract.

--- Rafael Caldera/1-Scraper.R
+++ Rafael Caldera/1-Scraper.R
@@ -32,18 +32,27 @@ for(i in 1:length(links)){
   # remove non-text entries
   text <- text[grepl("[A-z]", text)]
   # remove excess white space
-  text <- str_replace_all(text, "^ +| +$|( ) +", "\\1") 
+  text <- str_replace_all(text, "^ +| +$|( ) +", "\\1")
+  # if date appears in text, extract
+  date <- NA
+  if(grepl("\\d{2}\\s{1}[A-z]+\\s{1}[A-z]+\\s{1}[A-z]+\\s{1}\\d{4}", text[1])){ # if a date appears in the first line of the text
+  date <- str_extract(text[1], "\\d{2}\\s{1}[A-z]+\\s{1}[A-z]+\\s{1}[A-z]+\\s{1}\\d{4}") # extract the date
+  text <- text[2:length(text)] # remove first line (presence of date indicates first line is descriptive)
+  }
   # collapse
   text <- paste(text, collapse = " ")
   # extract title and description
   html <- htmlParse(url, useInternalNodes=T)
   title <- unname(unlist(html['//meta[@property="og:title"]/@content']))
   description <- unname(unlist(html['//meta[@property="og:description"]/@content']))
-  # extract date if available
+  # extract year from title
   year <- str_extract(title, "\\d{4}") # find 4 consecutive digits in title
-  date <- str_extract(description, "\\d{2}\\s{1}[A-z]+\\s{1}[A-z]+\\s{1}[A-z]+\\s{1}\\d{4}")
+  # if date was not in text, check if it is in the description
+  if(is.na(date)){date <- str_extract(description, "\\d{2}\\s{1}[A-z]+\\s{1}[A-z]+\\s{1}[A-z]+\\s{1}\\d{4}")} # find date in description
   #digit_present <- unlist(str_split(description, " ")) %>% grepl("[[:digit:]]",.)
   #date <- unlist(str_split(description, " ")) %>% .[which(digit_present == TRUE)[1]:which(digit_present == TRUE)[2]] %>% paste(., collapse = " ")
+  # if year is missing, extract from date (will retunr NA if it's also missing from date)
+  if(is.na(year)){str_extract(date, "\\d{4}")}
   caldera_corpus <- rbind(caldera_corpus, data.table("title" = title, "description" = description, "year" = year, "date" = date, "text" = text))
 }
 
