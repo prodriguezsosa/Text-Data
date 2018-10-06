0c983eaac5ec3f80fef8b84b143266dc7f4b56ee
prodriguezsosa <pedro.rodriguezsosa@gmail.com>
2018-10-05 20:07:42 -0400

    lapply script

--- Spanish Legislature/Scraper.R
+++ Spanish Legislature/Scraper.R
@@ -149,29 +149,6 @@ start_time <- Sys.time() # this takes about 1hr on my old mac pro laptop
 for(i in 1:length(links_all)){
   links_i <- links_all[[i]]
   #links_i <- links_i[1:5]
-  # loop version
-  #corpus_i <- list()
-  #meta_i <- list()
-  #for(j in 1:length(links_i)){
-  #  url <- paste0(url_base, links_i[j])
-  #  page <- url %>% read_html()
-  #  # meta info
-  #  meta <- page %>% html_nodes(xpath = '//*[(@id = "CABECERA_TEXTO_POPUP")]') %>% html_text
-  #  meta <- meta[2] %>% convert_latin1(.) %>% str_replace_all("^ +| +$|( ) +", "\\1") 
-  #  # text 
-  #  text <- page %>% html_nodes(xpath = '//*[contains(concat( " ", @class, " " ), concat( " ", "texto_completo", " " ))]') %>% html_text
-  #  text <- convert_latin1(text) # convert special characters
-  #  text <- str_split(text, "\n\n") %>% unlist # separate by sentence
-  #  text <- str_replace_all(text, "\\n", " ") %>% unlist  # remove spacing symbols
-  #  text <- str_replace_all(text, "^ +| +$|( ) +", "\\1")  # remove excess white space
-  #  # save
-  #  corpus_i[[j]] <- text
-  #  meta_i[[j]] <- meta
-  #  #Sys.sleep(2)
-  #}
-  #corpus_text[[i]] <- corpus_i
-  #corpus_meta[[i]] <- meta_i
-  # lapply version
   corpus[[i]] <- pblapply(links_i, scrapeText)
   print(i)
 }
